af4c4832d209afb60e14bff47758ad1594e31bf6
amitGanguly19 <agangul1@my.centennialcollege.ca>
2022-06-18 19:46:26 -0400

    Initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+//iife
+//AKa - anonymous self- excuting function
+
+(function()
+{
+    let Start = function()
+    {
+        console.log("App Started!...");
+
+    }
+    window.addEventListener("Load", Start);
+
+})();
